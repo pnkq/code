cf3b061ec772fdc7467b07fd804b384b592d8bea
phuonglh <phuonglh@vnu.edu.vn>
2024-07-28 17:16:24 +0700

    u

---
+++ lhp/src/main/java/lhp/Talk.java
@@ -0,0 +1,31 @@
+package lhp;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+
+@Entity
+public class TalkEntity implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false) @Lob
+    private @NotNull String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
