fe877bf48b1d4c8e83396bfb00dea46763169c98
phuonglh <phuonglh@gmail.com>
2024-11-22 19:02:47 +0700

    u

--- con/src/main/scala/vlp/ner/ConfigNER.scala
+++ con/src/main/scala/vlp/ner/ConfigNER.scala
@@ -13,7 +13,7 @@ case class ConfigNER(
   layers: Int = 2, // number of LSTM layers or BERT blocks
   hiddenSize: Int = 64,
   epochs: Int = 20,
-  learningRate: Double = 1E-5,
+  learningRate: Double = 5E-6,
   modelPath: String = "bin/med",
   trainPath: String = "dat/med/syll.txt",
   validPath: String = "dat/med/val/", // Parquet file of devPath
