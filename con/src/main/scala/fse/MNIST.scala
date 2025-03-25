b821c96aa44ffe62d43fc0e433335bd9969e09e1
phuonglh <phuonglh@gmail.com>
2025-03-25 18:40:02 +0700

    u

--- con/src/main/scala/fse/MNIST.scala
+++ con/src/main/scala/fse/MNIST.scala
@@ -1,6 +1,5 @@
 package fse
 
-import com.intel.analytics.bigdl.dllib.utils.Engine
 import org.apache.spark.SparkContext
 import org.apache.spark.ml.classification.MultilayerPerceptronClassifier
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
@@ -11,9 +10,20 @@ import org.apache.spark.sql.functions._
 import java.awt.image.DataBufferByte
 import java.io.ByteArrayInputStream
 import javax.imageio.ImageIO
+import com.intel.analytics.bigdl.dllib.utils.Engine
+import com.intel.analytics.bigdl.numeric.NumericFloat
+import com.intel.analytics.bigdl.dllib.keras.layers._
+import com.intel.analytics.bigdl.dllib.utils.Shape
+import com.intel.analytics.bigdl.dllib.keras.Sequential
+import com.intel.analytics.bigdl.dllib.keras.optimizers.Adam
+import com.intel.analytics.bigdl.dllib.nn.ClassNLLCriterion
+import com.intel.analytics.bigdl.dllib.nnframes.NNEstimator
+import com.intel.analytics.bigdl.dllib.optim.{Loss, Top1Accuracy, Top5Accuracy, Trigger}
+import com.intel.analytics.bigdl.dllib.visualization.{TrainSummary, ValidationSummary}
 
 /**
  * (C) phuonglh@gmail.com
+ *
  */
 object MNIST {
   private val numCores = Runtime.getRuntime.availableProcessors()
@@ -26,12 +36,24 @@ object MNIST {
   })
 
   private def ffn(train: DataFrame, test: DataFrame): Unit = {
-    val classifier = new MultilayerPerceptronClassifier().setLayers(Array(784, 128, 10))
+    val classifier = new MultilayerPerceptronClassifier().setLayers(Array(784, 100, 10))
     val model = classifier.fit(train)
     val (uf, vf) = (model.transform(train), model.transform(test))
     val evaluator = new MulticlassClassificationEvaluator()
     val (a, b) = (evaluator.evaluate(uf), evaluator.evaluate(vf))
     println(s"training score = $a, test score = $b")
+  }
+
+  private def cnn: Sequential[Float] = {
+    val model = Sequential()
+    model.add(Reshape(Array(1, 28, 28), inputShape = Shape(28 * 28)))
+    model.add(Convolution2D(6, 5, 5, activation = "tanh").setName("conv1_5x5"))
+    model.add(MaxPooling2D())
+    model.add(Convolution2D(12, 5, 5, activation = "tanh").setName("conv2_5x5"))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(100, activation = "tanh").setName("fc1"))
+    model.add(Dense(10, activation = "softmax").setName("fc2"))
   }
 
   def main(args: Array[String]): Unit = {
@@ -48,7 +70,25 @@ object MNIST {
       spark.read.parquet("dat/mnist/test-00000-of-00001.parquet").withColumn("features", decode(col("image.bytes")))
     )
 
-    ffn(train, test)
+    // 1. FFN
+//    ffn(train, test)
+
+    // 2. CNN
+    val inc = udf((i: Long) => (i + 1d))
+    val (uf, vf) = (
+      train.withColumn("number", inc(col("label"))),
+      test.withColumn("number", inc(col("label")))
+    )
+    val criterion = ClassNLLCriterion(sizeAverage = false, logProbAsInput = false)
+    val estimator = NNEstimator(cnn, criterion, Array(28*28), Array(1))
+    val trainingSummary = TrainSummary(appName = "cnn", logDir = "sum/cnn/")
+    val validationSummary = ValidationSummary(appName = "cnn", logDir = "sum/cnn/")
+    val batchSize = numCores * 4
+    estimator.setBatchSize(batchSize).setOptimMethod(new Adam(2E-4)).setMaxEpoch(40)
+      .setLabelCol("number")
+      .setTrainSummary(trainingSummary).setValidationSummary(validationSummary)
+      .setValidation(Trigger.everyEpoch, vf, Array(new Top1Accuracy, new Top5Accuracy, new Loss), batchSize)
+    estimator.fit(uf)
 
     spark.stop()
   }
