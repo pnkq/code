eca32a8f482ecf71bf4ea4308f3796a51cca8c06
phuonglh <phuonglh@gmail.com>
2026-01-22 22:16:52 +0700

    u

--- con/src/main/scala/vlp/ner/Oracle.scala
+++ con/src/main/scala/vlp/ner/Oracle.scala
@@ -7,13 +7,13 @@ import java.util.concurrent.atomic.AtomicInteger
 /**
   * Created by phuonglh on January 9, 2026.
   * 
-  * A parsing context contains a stack, a queue, and bag-of-features 
+  * A parsing context contains a stack, a queue, an output queue and bag-of-features 
   *  (space-separated feature strings) and a transition label.
   * 
   */
 case class Context(id: Int, bof: String, transition: String, 
-  stack: Stack[String], queue: Queue[String], 
-    words: Seq[String], tags: Seq[String], pastTransitions: Seq[String]) {
+  stack: Stack[Int], queue: Queue[Int], 
+    words: Seq[String], actions: Seq[String]) {
   override def toString: String = {
     val sb = new StringBuilder()
     sb.append('(')
@@ -27,21 +27,79 @@ case class Context(id: Int, bof: String, transition: String,
     sb.append(')')
     sb.append(" => ")
     sb.append(transition)
-    // sb.append(',')
-    // sb.append(pastTransitions)
+    sb.append(',')
+    sb.append(actions)
     sb.toString()
   }
 }
 
 /**
-  * Oracle is a key component in training transition-based NER. It is used to derive 
-  * optimal transition sequences from a gold sequence.
+  * Oracle is a key component in training transition-based NER. It is used to derive an
+  * optimal transition sequence from a named entity tagged sequence.
   *
   */
 object Oracle {
   val counter = new AtomicInteger(0)
 
-  def decode(words: Seq[String], labels: Seq[String]): List[Context] = {
-    ???
+  def decode(words: Seq[String], spans: Map[(Int, Int), String]): List[Context] = {
+    val n = words.size
+    val queue = Queue[Int]()
+    for (i <- 0 until words.length) queue.enqueue(i)
+    val stack = Stack[Int]()
+    var actions = ListBuffer[String]()
+    var contexts = ListBuffer[Context]()
+    var i = 0
+    var action = "NA"
+    while (queue.nonEmpty || stack.nonEmpty) {
+      // case 1: stack forms a complete gold entity
+      if (stack.nonEmpty) {
+        val (u, v) = (stack.last, stack.top)
+        if (spans.contains((u, v))) {
+          val y = spans((u, v))
+          action = s"RE-$y"
+          contexts = contexts :+ Context(counter.incrementAndGet(), "bof", action, stack.clone(), queue.clone(), words, actions.toList)
+          stack.clear()
+        }
+      } 
+      if (action == "NA" && i < n) {
+        // case 2: queue is not empty and token belongs to an entity
+        if (spans.keySet.exists { case (l, r) => (l <= i) && (i <= r) }) {
+          action = "SH"
+          contexts = contexts :+ Context(counter.incrementAndGet(), "bof", action, stack.clone(), queue.clone(), words, actions.toList)
+          queue.dequeue()
+          stack.push(i)
+        } else {
+          action = "OUT"
+          contexts = contexts :+ Context(counter.incrementAndGet(), "bof", action, stack.clone(), queue.clone(), words, actions.toList)
+          queue.dequeue()
+        }
+        i = i + 1
+      }
+      actions = actions :+ action
+      action = "NA"
+    }
+    contexts.toList
   }
+
+  def main(args: Array[String]): Unit = {
+    val words = Seq("Mark", "Watney", "visited", "Mars")
+    val spans = Map((0, 1) -> "PER", (3, 3) -> "LOC")
+    val contexts = decode(words, spans)
+    contexts.foreach(println)
+  }
+
+
 }
+
+// repeat until (the stack and the buffer are both empty):
+// The SHIFT transition moves a word from the buffer directly to the stack
+// The OUT transition moves a word from the buffer directly into the output stack
+// The REDUCE-y transition pops all items from the top of the stack creating a "chunk", labels with this label y 
+//  and pushes a representation of this chunk onto the output stack.
+
+// At each step:
+//  If the stack matches a complete gold entity span → REDUCE
+//  Else if the next buffer token is part of a gold entity → SHIFT
+//  Else → OUT
+// This priority ordering is crucial.
+
