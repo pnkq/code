e5d737248f6205420316c2b1bf5d97ba92baee5b
phuonglh <phuonglh@vnu.edu.vn>
2026-02-17 23:10:29 +0700

    650M

--- con/src/main/python/solubility.py
+++ con/src/main/python/solubility.py
@@ -8,13 +8,17 @@ import pyarrow.parquet as pq
 # 1. Load the model and tokenizer
 # Smaller models: 'esm2_t6_8M_UR50D', 'esm2_t12_35M_UR50D'
 # Larger models: 'esm2_t30_150M_UR50D', 'esm2_t33_650M_UR50D'
-model_name = "facebook/esm2_t6_8M_UR50D"
+#model_name = "facebook/esm2_t6_8M_UR50D"
+model_name = "facebook/esm2_t33_650M_UR50D"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = EsmModel.from_pretrained(model_name)
 
 # 2. Load protein sequences from a CSV file
 
-df = pd.read_csv("/Users/phuonglh/code/con/dat/sol/S.cerevisiae_test.csv")
+#file_name = "S.cerevisiae_test"
+#file_name = "eSol_train"
+file_name = "eSol_test"
+df = pd.read_csv(f"/Users/phuonglh/code/con/dat/sol/{file_name}.csv")
 
 # Basic Data Cleaning
 # It's a good idea to remove any rows with missing sequences 
@@ -74,7 +78,7 @@ metadata_df = df[['gene', 'solubility']].reset_index(drop=True)
 final_df = pd.concat([metadata_df, embedding_df], axis=1)
 
 # 4. Save to Parquet
-final_df.to_parquet("/Users/phuonglh/code/con/dat/sol/S.cerevisiae_test_embeddings.parquet", engine='pyarrow', compression='snappy')
+final_df.to_parquet(f"/Users/phuonglh/code/con/dat/sol/{file_name}_embeddings_650M.parquet", engine='pyarrow', compression='snappy')
 
 print(f"Successfully saved to Parquet! File size is roughly {final_df.memory_usage().sum() / 1e6:.2f} MB")
 
--- con/src/main/scala/sol/Solubility.scala
+++ con/src/main/scala/sol/Solubility.scala
@@ -14,6 +14,7 @@ import org.apache.spark.ml.regression.GBTRegressionModel
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.ml.regression.Regressor
 import org.apache.spark.ml.regression.LinearRegressionModel
+import breeze.linalg.dim
 
 /**
  * (C) phuonglh@gmail.com
@@ -28,14 +29,15 @@ object RegressorType extends Enumeration {
 
 object Solubility {
 
-  def run(uf: DataFrame, vf: DataFrame, esm: Boolean = false, t: RegressorType.Value = RegressorType.RANDOM_FOREST, verbose: Boolean = false) = {
+  def run(uf: DataFrame, vf: DataFrame, wf: DataFrame, esm: Boolean = false, dimensions: Int = 320,
+    t: RegressorType.Value = RegressorType.RANDOM_FOREST, verbose: Boolean = false) = {
     val regressor = t match {
       case RegressorType.RANDOM_FOREST =>
         new RandomForestRegressor().setLabelCol("solubility").setNumTrees(128)
       case RegressorType.GRADIENT_BOOSTED_TREE =>
         new GBTRegressor().setLabelCol("solubility")
       case RegressorType.LINEAR_REGRESSION => 
-        new LinearRegression().setLabelCol("solubility").setRegParam(1E-4)
+        new LinearRegression().setLabelCol("solubility")
       case _ =>
         new RandomForestRegressor().setLabelCol("solubility").setNumTrees(128)
     }
@@ -43,7 +45,7 @@ object Solubility {
       val hashing = new HashingTF().setInputCol("tokens").setOutputCol("features").setNumFeatures(32)
       new Pipeline().setStages(Array(hashing, regressor))
     } else {
-      val columns = (0 until 320).map(j => s"dim_$j").toArray
+      val columns = (0 until dimensions).map(j => s"dim_$j").toArray
       val assembler = new VectorAssembler().setInputCols(columns).setOutputCol("features")
       new Pipeline().setStages(Array(assembler, regressor))
     }
@@ -51,6 +53,7 @@ object Solubility {
     val model = pipeline.fit(uf)
     val af = model.transform(uf)
     val bf = model.transform(vf)
+    val cf = model.transform(wf)
     af.select("prediction", "solubility", "features").show(5)
 
     if (verbose) {
@@ -71,10 +74,12 @@ object Solubility {
       }
     }
     val evaluator = new RegressionEvaluator().setLabelCol("solubility").setPredictionCol("prediction").setMetricName("r2")
-    var metric = evaluator.evaluate(bf)
-    println(s" R^2 on test data = $metric")
-    metric = evaluator.evaluate(af)
+    var metric = evaluator.evaluate(af)
     println(s"R^2 on train data = $metric")
+    metric = evaluator.evaluate(bf)
+    println(s"R^2 on valid data = $metric")
+    metric = evaluator.evaluate(cf)
+    println(s" R^2 on test data = $metric")
   }
 
   def main(args: Array[String]): Unit = {
@@ -84,31 +89,32 @@ object Solubility {
     val basePath = "dat/sol/"
 
     val esm: Boolean = true
+    val dimensions = 320 // 1280 // 320
     val regressorType = RegressorType.LINEAR_REGRESSION
     println(s"esm = $esm, regressorType = $regressorType")
 
-    val (uf, vf) = if (!esm) {
+    val (uf, vf, wf) = if (!esm) {
       val paths = Array("eSol_train", "eSol_test", "S.cerevisiae_test").map(p => s"$basePath/$p.csv")
       val train = spark.read.option("header", value = true).option("inferSchema", true).csv(paths(0))
       val valid = spark.read.option("header", value = true).option("inferSchema", true).csv(paths(1))
+      val test = spark.read.option("header", value = true).option("inferSchema", true).csv(paths(2))
       train.show(5)
       val f = udf((seq: String) => seq.toCharArray().map(_.toString()))
       val uf = train.withColumn("tokens", f(col("sequence")))
       val vf = valid.withColumn("tokens", f(col("sequence")))
+      val wf = test.withColumn("tokens", f(col("sequence")))
       uf.select("solubility", "tokens").show(5)
-      (uf, vf)
+      (uf, vf, wf)
     } else {
-      val paths = Array("eSol_train", "eSol_test", "S.cerevisiae_test").map(p => s"$basePath/${p}_embeddings.parquet")
+      val suffix = if (dimensions == 320) "8M" else "650M"
+      val paths = Array("eSol_train", "eSol_test", "S.cerevisiae_test").map(p => s"$basePath/${p}_embeddings_${suffix}.parquet")
       val uf = spark.read.parquet(paths(0))
       val vf = spark.read.parquet(paths(1))
-      (uf, vf)
+      val wf = spark.read.parquet(paths(2))
+      (uf, vf, wf)
     }
-    // // Exp 1. ESM with RF (320 features)
-    // run(uf, vf, esm, regressorType, false)
-    // // Exp 2. ESM with GBT (320 features)
-    // run(uf, vf, esm, regressorType, false)
-    // Exp 3. ESM with LR (320 features)
-    run(uf, vf, esm, regressorType, true)
+    // 
+    run(uf, vf, wf, esm, dimensions, regressorType, false)
 
     spark.stop()
   }
