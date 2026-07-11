9c8d5e8f1358359ae129e6d6a673e80dbdc08be8
phuonglh <phuonglh@gmail.com>
2026-07-11 15:34:45 +0700

    filter vocab when loading

--- con/src/main/python/ptok/main.py
+++ con/src/main/python/ptok/main.py
@@ -13,15 +13,17 @@ pipeline = Pipeline()
 #     print(p)
 
 # build and save the vocab
-corpus_dir = "20231101_vie"
+# corpus_dir = "20231101_vie"
+# reader = CorpusReader(corpus_dir)
+# builder = VocabularyBuilder(pipeline)
+# vocab = builder.build(reader)
+# vocab.save("{}.json".format(corpus_dir))
 
-reader = CorpusReader(corpus_dir)
-builder = VocabularyBuilder(pipeline)
-vocab = builder.build(reader)
-vocab.save("{}.json".format(corpus_dir))
+vocab = Vocabulary.load_and_prune("20231101_vie.json", 8)
+print("vocab_size = {}".format(len(vocab)))
+vocab.save("vocab.json")
 
-# vocab = Vocabulary.load("{}.json".format(corpus_dir))
-
+corpus_dir = "0"
 builder = DatasetBuilder(pipeline, vocab, "{}".format(corpus_dir), max_length=512)
 builder.save("{}.npy".format(corpus_dir))
 print("   pieces = {}".format(builder.stats.pieces))
