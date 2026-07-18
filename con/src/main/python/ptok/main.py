adb30d675eb865ee851b157d21d6150ec699842f
phuonglh <phuonglh@gmail.com>
2026-07-18 10:24:23 +0700

    update main to use arguments

--- con/src/main/python/ptok/main.py
+++ con/src/main/python/ptok/main.py
@@ -1,76 +1,109 @@
-import unicodedata
 from p.vocabulary import VocabularyBuilder, Vocabulary
 from p.pipeline import Pipeline
 from t.dataset import CorpusReader, DatasetBuilder
 from p.tokenizer import HybridTokenizer
 from t.memmap import MemMapWriter, MemMapDataset
 
+import argparse
+import sys
 
-pipeline = Pipeline()
+def tokenize_simple_text(pipeline):
+    """Test 1: Tokenize a simple text."""
+    text = """Tôi đang học transformers của OpenAI và SpaceX."""
+    pieces = pipeline.tokenize(text)
+    for p in pieces:
+        print(p)
 
-# TEST 1: tokenize a simple text
-# text = """Tôi đang học transformers của OpenAI và SpaceX."""
-# pieces = pipeline.tokenize(text)
-# for p in pieces:
-#     print(p)
+def build_vocabulary(corpus_dir):
+    """Test 2: Build and save a vocabulary."""
+    reader = CorpusReader(corpus_dir)
+    pipeline = Pipeline()
+    builder = VocabularyBuilder(pipeline)
+    vocab = builder.build(reader)
+    vocab.save("{}.json".format(corpus_dir))
 
-# TEST 2: build and save a vocab
-# corpus_dir = "20231101_vie"
-# reader = CorpusReader(corpus_dir)
-# pipeline = Pipeline()
-# builder = VocabularyBuilder(pipeline)
-# vocab = builder.build(reader)
-# vocab.save("{}.json".format(corpus_dir))
+def prune_vocabulary(input_vocab_file, output_vocab_file="vocab.json", min_frequency=8):
+    """Test 3: Load and prune a vocabulary, reindex entry ids; the output is 'vocab.json'."""
+    vocab = Vocabulary.load_and_prune(input_vocab_file, min_frequency)
+    print(f"vocab_size = {len(vocab)}")
+    vocab.save(output_vocab_file)
 
-# TEST 3: load and prune a vocab
-# vocab = Vocabulary.load_and_prune("20231101_vie.json", 8)
-# print("vocab_size = {}".format(len(vocab)))
-# vocab.save("vocab.json")
+def tokenize(pipeline, vocab_file="vocab.json"):
+    """Test 4: Test a hybrid tokenizer using a pre-built vocabulary."""
+    vocab = Vocabulary.load(vocab_file)
+    tokenizer = HybridTokenizer(pipeline, vocab)
+    text = """Tôi đang học transformers của OpenAI."""
+    result = tokenizer.encode(text)
+    print(result)
+    print()
+    result2 = tokenizer(text)
+    print(result2)
+    print()
+    result3 = tokenizer([text, "Thực hiện theo ý kiến chỉ đạo của Giám đốc Đại học Quốc gia Hà Nội"])
+    print(result3)
 
-# TEST 4: test the HybridTokenizer
-vocab = Vocabulary.load("vocab.json")
-tokenizer = HybridTokenizer(pipeline, vocab)
-# text = """Tôi đang học transformers của OpenAI."""
-# result = tokenizer.encode(text)
-# print(result)
-# print()
-# result2 = tokenizer(text)
-# print(result2)
-# print()
-# result3 = tokenizer([text, "Thực hiện theo ý kiến chỉ đạo của Giám đốc Đại học Quốc gia Hà Nội"])
-# print(result3)
+def dataset_builder(tokenizer, corpus_dir):
+    """Test 5: Convert a corpus into sequences of ids."""
+    builder = DatasetBuilder(tokenizer, "{}".format(corpus_dir), sequence_length=32)
+    seqs = builder.build()
+    for s in seqs:
+        print(s)
+    print("   pieces = {}".format(builder.stats.pieces))
+    print("    lines = {}".format(builder.stats.lines))
+    print(" sequences = {}".format(builder.stats.sequences))
+    
+def memmap_writer(tokenizer, corpus_dir, sequence_length):
+    """Test 6: Save the corpus into a *.bin file for training, then load the binary dataset into a mem-map dataset."""
+    builder = DatasetBuilder(tokenizer, f"{corpus_dir}", sequence_length=sequence_length)
+    writer = MemMapWriter(f"{corpus_dir}.bin", sequence_length=sequence_length+2)
 
-# TEST 5: test the dataset builder that convert a corpus into sequences of ids
-# corpus_dir = "0"
-# builder = DatasetBuilder(tokenizer, "{}".format(corpus_dir), sequence_length=32)
-# seqs = builder.build()
-# for s in seqs:
-#     print(s)
-# # builder.save("{}.npy".format(corpus_dir))
-# print("   pieces = {}".format(builder.stats.pieces))
-# print("    lines = {}".format(builder.stats.lines))
-# print(" sequences = {}".format(builder.stats.sequences))
+    for seq in builder.build():
+        writer.write(seq)
 
+    writer.close()
+    
+def memmap_dataset(corpus_dir, sequence_length):
+    dataset = MemMapDataset(f"{corpus_dir}.bin",  sequence_length=sequence_length+2)
+    print(f"Number of sequences = {len(dataset)}")
+    sample = dataset[0]
+    print(f'Shape of a sequence is {sample["input_ids"].shape}')
+        
 
-# TEST 6: build memmap writer and save the corpus into a *.bin file for training,
-# then load the binary dataset into a mem-map dataset. 
-#corpus_dir = "0"
-corpus_dir = "1"
-#corpus_dir = "20231101_vie"
+def main():
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(
+        description="Run a specific function based on a command-line argument."
+    )
+    
+    # Define the choices available to the user
+    parser.add_argument(
+        'action',
+        choices=['tokenize', 'vocab', 'memmap', 'dataset'],
+        help="The specific function/action you want to execute."
+    )
 
-sequence_length = 512
+    # Parse the arguments from command line.
+    args = parser.parse_args()
+    pipeline = Pipeline()
 
-# builder = DatasetBuilder(tokenizer, "{}".format(corpus_dir), sequence_length=sequence_length)
-# writer = MemMapWriter("{}.bin".format(corpus_dir), sequence_length=sequence_length+2)
-# for seq in builder.build():
-#     writer.write(seq)
+    match args.action:
+        case 'tokenize': 
+            tokenize(pipeline)
+        case 'vocab': 
+            build_vocabulary("20231101_vie")
+        case 'prune':
+            prune_vocabulary("20231101_vie", "vocab.json")
+        case 'memmap': 
+            tokenizer = HybridTokenizer(pipeline, Vocabulary.load("vocab.json"))
+            memmap_writer(tokenizer, "1", 512)
+            memmap_dataset("1", 512)
+        case 'dataset': 
+            tokenizer = HybridTokenizer(pipeline, Vocabulary.load("vocab.json"))
+            dataset_builder(tokenizer, "0")
+        case _:
+            print("Invalid action selection.", file=sys.stderr)
 
-# writer.close()
-
-dataset = MemMapDataset("{}.bin".format(corpus_dir),  sequence_length=sequence_length+2)
-print(f"Number of sequences = {len(dataset)}")
-sample = dataset[0]
-print(f'Shape of a sequence is {sample["input_ids"].shape}')
-
+if __name__ == "__main__":
+    main()
 
 
