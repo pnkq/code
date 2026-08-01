23c089e45c570d954d3c143edd11b4d9fe15a8ca
phuonglh <phuonglh@gmail.com>
2026-08-01 13:14:23 +0700

    update paths

--- ptok/m/train.py
+++ ptok/m/train.py
@@ -6,9 +6,6 @@ from pathlib import Path
 HOME_DIR = Path.home()
 sys.path.append(f"{HOME_DIR}/code/ptok/")
 
-# import os
-# os.environ["CUDA_VISIBLE_DEVICES"] = ""
-
 from m.config import TrainingConfig
 from t.memmap import MemMapDataset
 from t.collator import MaskedLanguageModelDataCollator
@@ -21,8 +18,7 @@ def main():
 
     tokenizer = HybridTokenizer(Pipeline(), Vocabulary.load("vocab.json"))
 
-    # dataset = MemMapDataset("20231101_vie.bin", sequence_length=512)
-    dataset = MemMapDataset("2.bin", sequence_length=512)
+    dataset = MemMapDataset("w.bin", sequence_length=512)
     print(f"Vocab size: {len(tokenizer)}")
     print(f"Number of sequences: {dataset.num_sequences}")
     print(f"Shape of a sequence: {dataset[0]['input_ids'].shape}")
